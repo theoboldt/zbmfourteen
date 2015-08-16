5736bdc23360b935421bb9d7d0c154930a39c3c9
theoboldt <e.s.t.m52@gmx.de>
2015-08-16 14:00:38 +0200

    Improved caching for script/style

--- style.php
+++ style.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'inc/last_modified.php';
 
+header("Cache-Control: public, max-age=90000, s-maxage=9000");
 header('Content-type: text/css', true);
 $filename	= 'style.min.css';
 
