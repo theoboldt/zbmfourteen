1a96047b0b578744d0831bf8b3b101bab18d8c7c
theoboldt <e.s.t.m52@gmx.de>
2015-08-16 14:02:23 +0200

    Improved caching for script/style

--- script.php
+++ script.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'inc/last_modified.php';
 
-header("Cache-Control: public, max-age=90000, s-maxage=9000");
+header("Cache-Control: public, max-age=1209600, s-maxage=1209600");
 header('Content-type: application/javascript; charset=utf-8', true);
 $filename	= 'script.min.js';
 
