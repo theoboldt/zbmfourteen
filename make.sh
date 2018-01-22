9667aa2b23a186cbabe6bc540b080f880e338e7e
theoboldt <erik@theoboldt.eu>
2018-01-22 22:48:36 +0100

    Updated build script

--- make.sh
+++ make.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env bash
-
-echo "Compiling LESS..."
-lessc less/style.less > style.css
-lessc less/style-login.less > style-login.css
-echo "LESS done."
-
-echo "Compressing CSS..."
-/usr/bin/yui-compressor -o style.min.css style.css
-echo "CSS done."
-
 echo "Collecting JS..."
-echo "" >> script.min.js.tmp
+echo "" > script.min.js.tmp
 
 cat "js/jquery.countdown.min.js" >> script.min.js.tmp
+echo "" >> script.min.js.tmp
 
+cat "js/bootstrap/modal.js" >> script.min.js.tmp
+echo "" >> script.min.js.tmp
 cat "js/bootstrap/carousel.js" >> script.min.js.tmp
+echo "" >> script.min.js.tmp
 cat "js/bootstrap/transition.js" >> script.min.js.tmp
+echo "" >> script.min.js.tmp
+cat "js/ekko-lightbox-2.6.0.min.js" >> script.min.js.tmp
+echo "" >> script.min.js.tmp
 
 cat "js/functions.js" >> script.min.js.tmp
+echo "" >> script.min.js.tmp
 
 #cat "js/tiled-gallery/jquery.spin.js" >> script.min.js.tmp
 #cat "js/tiled-gallery/spin.js" >> script.min.js.tmp
@@ -25,7 +23,19 @@ cat "js/functions.js" >> script.min.js.tmp
 #cat "js/tiled-gallery/tiled-gallery.js" >> script.min.js.tmp
 
 echo "Compressing JS..."
-/usr/bin/yui-compressor --type=js -o script.min.js script.min.js.tmp
+#/usr/bin/yui-compressor --type=js -o script.min.js script.min.js.tmp
+/usr/local/bin/uglifyjs -o script.min.js script.min.js.tmp
 echo "Removing temporary file..."
-rm script.min.js.tmp
+#rm script.min.js.tmp
 echo "JS done."
+
+exit
+
+echo "Compiling LESS..."
+lessc less/style.less > style.css
+lessc less/style-login.less > style-login.css
+echo "LESS done."
+
+echo "Compressing CSS..."
+#/usr/bin/yui-compressor -o style.min.css style.css
+echo "CSS done."
